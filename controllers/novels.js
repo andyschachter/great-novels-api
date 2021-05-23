a5d5fbf727cd6da1836823b227559c72b4765e95
Andy Schachter <andy.schachter@gmail.com>
2021-05-23 18:50:33 -0400

    added route and code for getNovelById

--- controllers/novels.js
+++ controllers/novels.js
@@ -15,6 +15,25 @@ const getAllNovels = async (request, response) => {
   }
 }
 
+const getNovelById = async (request, response) => {
+  try {
+    const { id } = request.params
+
+    const novel = await models.Novels.findOne({
+      where: { id },
+      include: [{
+        model: models.Authors
+      },
+      { model: models.Genres }]
+    })
+
+    return novel ? response.send(novel) : response.sendStatus(404)
+  } catch (error) {
+    return response.status(500).send('Unable to retrieve genre')
+  }
+}
+
 module.exports = {
-  getAllNovels
+  getAllNovels,
+  getNovelById
 }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { getAllAuthors, getAuthorById } = require('./controllers/authors')
 const { getAllGenres, getGenreById } = require('./controllers/genres')
-const { getAllNovels } = require('./controllers/novels')
+const { getAllNovels, getNovelById } = require('./controllers/novels')
 
 const app = express()
 
@@ -14,6 +14,8 @@ app.get('/genres', getAllGenres)
 app.get('/genres/:id', getGenreById)
 
 app.get('/novels', getAllNovels)
+
+app.get('/novels/:id', getNovelById)
 
 app.listen(1347, () => {
   console.log('Listening on port 1347...') // eslint-disable-line no-console
