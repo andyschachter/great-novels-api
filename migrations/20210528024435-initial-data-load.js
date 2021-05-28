b073730600e14be30e3c263abba4c88e25fe09e3
Andy Schachter <andy.schachter@gmail.com>
2021-05-27 22:58:07 -0400

    added table load info for novels table

--- migrations/20210528024435-initial-data-load.js
+++ migrations/20210528024435-initial-data-load.js
@@ -6,6 +6,7 @@ module.exports = {
       { nameFirst: 'Agatha', nameLast: 'Christie' },
       { nameFirst: 'Alexandre', nameLast: 'Dumas' },
       { nameFirst: 'Alice', nameLast: 'Walker' },
+      { nameFirst: 'Arthur', nameLast: 'Conan Doyle' },
       { nameFirst: 'Arthur', nameLast: 'Miller' },
       { nameFirst: 'Bram', nameLast: 'Stoker' },
       { nameFirst: 'Charles', nameLast: 'Dickens' },
@@ -19,7 +20,23 @@ module.exports = {
       { nameFirst: 'Robert', nameLast: 'Louis Stevenson' }
     ])
 
-    await queryInterface.bulkInsert('novels',)
+    await queryInterface.bulkInsert('novels', [
+      { title: 'Murder on the Orient Express', authorId: 1 },
+      { title: 'The Three Musketeers', authorId: 2 },
+      { title: 'The Color Purple', authorId: 3 },
+      { title: 'The Hound of the Baskervilles', authorId: 4 },
+      { title: 'The Crucible', authorId: 5 },
+      { title: 'Dracula', authorId: 6 },
+      { title: 'A Tale of Two Cities', authorId: 7 },
+      { title: 'Things Fall Apart', authorId: 8 },
+      { title: 'Crime and Punishment', authorId: 9 },
+      { title: 'Animal Farm', authorId: 10 },
+      { title: 'The Time Machine', authorId: 11 },
+      { title: 'War and Peace', authorId: 12 },
+      { title: 'The Picture of Dorian Gray', authorId: 13 },
+      { title: 'Fahrenheit 451', authorId: 14 },
+      { title: 'The Strange Case of Dr. Jekyll and Mr. Hyde', authorId: 15 },
+    ])
   },
 
   down: async (queryInterface, Sequelize) => {
