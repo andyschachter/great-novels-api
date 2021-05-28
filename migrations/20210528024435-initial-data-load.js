1742db17a68d4c186930de8f52daa5cebf0d4fae
Andy Schachter <andy.schachter@gmail.com>
2021-05-27 22:51:35 -0400

    added table load info for authors table

--- migrations/20210528024435-initial-data-load.js
+++ migrations/20210528024435-initial-data-load.js
@@ -2,12 +2,24 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.bulkInsert('authors', [
+      { nameFirst: 'Agatha', nameLast: 'Christie' },
+      { nameFirst: 'Alexandre', nameLast: 'Dumas' },
+      { nameFirst: 'Alice', nameLast: 'Walker' },
+      { nameFirst: 'Arthur', nameLast: 'Miller' },
+      { nameFirst: 'Bram', nameLast: 'Stoker' },
+      { nameFirst: 'Charles', nameLast: 'Dickens' },
+      { nameFirst: 'Chinua', nameLast: 'Achebe' },
+      { nameFirst: 'Fyodor', nameLast: 'Dostoyevsky' },
+      { nameFirst: 'George', nameLast: 'Orwell' },
+      { nameFirst: 'H.G.', nameLast: 'Wells' },
+      { nameFirst: 'Leo', nameLast: 'Tolstoy' },
+      { nameFirst: 'Oscar', nameLast: 'Wilde' },
+      { nameFirst: 'Ray', nameLast: 'Bradbury' },
+      { nameFirst: 'Robert', nameLast: 'Louis Stevenson' }
+    ])
+
+    await queryInterface.bulkInsert('novels',)
   },
 
   down: async (queryInterface, Sequelize) => {
